e8dacaa5aa96c925f16060b078152450e7a3832e
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2014-11-13 11:08:13 +0100

    Step-up infrasturcture WIP

--- filter_plugins/custom_plugins.py
+++ filter_plugins/custom_plugins.py
@@ -1,11 +1,13 @@
 #
 # Usage: {{ foo | vault }}
-#
+#        {{ foo | sha256 }}
+
 def vault(encrypted):
   method = """
 from keyczar import keyczar
 import os.path
 import sys
+
 keydir = os.path.expanduser('~/.stepup-ansible-keystore')
 crypter = keyczar.Crypter.Read(keydir)
 sys.stdout.write(crypter.Decrypt("%s"))
@@ -13,9 +15,17 @@ sys.stdout.write(crypter.Decrypt("%s"))
   from subprocess import check_output
   return check_output(["python", "-c", method])
 
+
+def sha256s(data):
+  import hashlib
+  return hashlib.sha256(data).hexdigest()
+
+
 class FilterModule(object):
 
   def filters(self):
     return {
-      'vault': vault
+      'vault': vault,
+
+      'sha256': sha256s,
     }

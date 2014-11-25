2b585a0009705c5df7dad4143194c836db59dbab
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2014-11-25 12:12:57 +0100

    - Replace BSDish date invocation with some PHP - Add error handling

--- scripts/stepup-build.sh
+++ scripts/stepup-build.sh
@@ -20,15 +20,29 @@ fi
 cd ${CWD}/${REPO}
 COMMIT_HASH=`git log -1 --pretty="%H"`
 COMMIT_DATE=`git log -1 --pretty="%cd" --date=iso`
-COMMIT_Z_DATE=`date -u -j -f "%Y-%m-%d %H:%M:%S %z" "${COMMIT_DATE}" +"%Y%m%d%H%M%SZ"`
+COMMIT_Z_DATE=`php -r "echo gmdate('YmdHis\Z', strtotime('${COMMIT_DATE}'));"`
 COMMIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 NAME=${REPO}-${COMMIT_BRANCH}-${COMMIT_Z_DATE}-${COMMIT_HASH}
 
 composer.phar install --prefer-dist --no-scripts --ignore-platform-reqs --no-dev --no-interaction --optimize-autoloader
+if [ $? -ne "0" ]; then
+    echo "Composer install failed"
+    exit 1;
+fi
 
 TMP_ARCHIVE_DIR=`mktemp -d "/tmp/${REPO}.XXXXXXXX"`
+if [ $? -ne "0" ]; then
+    echo "Could not create temp dir"
+    exit 1;
+fi
 
 composer.phar archive --format=tar --dir="${TMP_ARCHIVE_DIR}" --no-interaction
+if [ $? -ne "0" ]; then
+    echo "Composer achive failed"
+    rm -r ${TMP_ARCHIVE_DIR}
+    exit 1;
+fi
+
 ARCHIVE_TMP_NAME=`find "${TMP_ARCHIVE_DIR}" -name "*.tar"`
 bzip2 -9 "${ARCHIVE_TMP_NAME}"
 mv ${ARCHIVE_TMP_NAME}.bz2 ${CWD}/${NAME}.tar.bz2

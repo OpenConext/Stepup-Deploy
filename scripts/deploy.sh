6daf5518586a00658a0af038452a1ff0f328b3cf
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2015-02-16 08:55:47 +0100

    - Add verbose option that is passed to ansible - Support diploying tars that are not stored in the currect directory - Test unpacking tar before deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 CWD=`pwd`
+BASEDIR=`dirname $0`
 COMPONENTS=("Stepup-Middleware" "Stepup-Gateway" "Stepup-SelfService" "Stepup-RA")
 UNARCHIVE=1
+VERBOSE=0
 
 function error_exit {
     echo "${1}"
+    if [ -n "${TMP_ARCHIVE_DIR}" -a -d "${TMP_ARCHIVE_DIR}" ]; then
+        rm -r "${TMP_ARCHIVE_DIR}"
+    fi
     cd ${CWD}
     exit 1
 }
@@ -30,30 +35,66 @@ case $option in
     -n|--no-unarchive)
     UNARCHIVE="0"
     ;;
+    -v|--verbose)
+    VERBOSE="1"
+    ;;
     *)
     error_exit "Unkown option: '${option}'"
     ;;
 esac
 done
 
+component_tarball_basename=`basename ${COMPONENT_TARBALL}`
 found=0
 for comp in "${COMPONENTS[@]}"; do
     regex="^($comp).*(\.tar\.bz2)$"
-    if [[ $COMPONENT_TARBALL =~ $regex ]]; then
+    if [[ $component_tarball_basename =~ $regex ]]; then
         found=1
         COMPONENT=$comp
     fi
 done
 if [ "$found" -ne "1" ]; then
-    error_exit "Component must end in .tar.bz2 and start with one of: ${COMPONENTS[*]}"
+    error_exit "Tarball to deploy must end in .tar.bz2 and start with one of: ${COMPONENTS[*]}"
 fi
 
 COMPONENT=`echo ${COMPONENT} | tr '[:upper:]' '[:lower:]'`
 echo "Deploying component: $COMPONENT"
-echo "Unarchive=${UNARCHIVE}"
+echo "unarchive=${UNARCHIVE}"
+echo "verbose=${VERBOSE}"
 
-cd Stepup-Deploy
+echo "Testing ${COMPONENT_TARBALL}"
 
-ansible-playbook deploy.yml --limit "app1*" --tags $COMPONENT -e "component_tarball_name=../$COMPONENT_TARBALL" -e "component_unarchive=${UNARCHIVE}"
+# Sanity check tar file
+TMP_ARCHIVE_DIR=`mktemp -d "/tmp/${COMPONENT}.XXXXXXXX"`
+if [ $? -ne "0" ]; then
+    error_exit "Could not create temp dir"
+fi
+
+# Extract tarball
+bunzip2 -k -q -c "${COMPONENT_TARBALL}" > ${TMP_ARCHIVE_DIR}/component.tar
+if [ $? -ne "0" ]; then
+    error_exit "bunzip2 failed"
+fi
+
+# Untar it
+tar -xf ${TMP_ARCHIVE_DIR}/component.tar
+if [ $? -ne "0" ]; then
+    error_exit "tar failed"
+fi
+
+rm -r ${TMP_ARCHIVE_DIR}
+
+
+# Start ansible deploy
+echo "Starting deploy"
+
+verbose_flag=""
+if [ "${VERBOSE}" -eq "1" ]; then
+    verbose_flag="-vvvv"
+fi
+
+playbook_root="${BASEDIR}/.."
+
+ansible-playbook ${playbook_root}/deploy.yml ${verbose_flag} -i ${playbook_root}/hosts --limit "app1*" --tags $COMPONENT -e "component_tarball_name=${COMPONENT_TARBALL}" -e "component_unarchive=${UNARCHIVE}"
 
 cd ${CWD}

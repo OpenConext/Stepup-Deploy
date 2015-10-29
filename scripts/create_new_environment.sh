af3fed90560f1a0daacf49478f8331727a20fb3e
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2015-10-29 17:53:41 +0100

    Add copyright and licence info

--- scripts/create_new_environment.sh
+++ scripts/create_new_environment.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# Creates a new environment that can be used with Ansible playbooks in Stepup-Deploy
+# Copyright 2015 SURFnet B.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Creates a new environment that can be used with Ansible playbooks
 # The script can be rerun in an existing environment and will not overwrite existing
 # files.
 # Please read the notice at the end of the script

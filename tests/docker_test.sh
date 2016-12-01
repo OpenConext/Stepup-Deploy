2ae507745946eeef87263041a483ad13c5444a8d
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2016-12-01 15:12:00 +0100

    Update MW and RA releases

--- tests/docker_test.sh
+++ tests/docker_test.sh
@@ -157,9 +157,9 @@ if [ ${SERVER} == "app" ]; then
     echo '===== Downloading component tarballs ====='
     tarballs=(
         "https://github.com/SURFnet/Stepup-Gateway/releases/download/2.2.0-20161018092553Z-bc6bbf8e2006d15cbe883d8045724cdb1166e759/Stepup-Gateway-2.2.0-20161018092553Z-bc6bbf8e2006d15cbe883d8045724cdb1166e759.tar.bz2"
-        "https://github.com/SURFnet/Stepup-Middleware/releases/download/2.1.0-20161026074332Z-6fade533b1516d4c13613cf91ac4963b6919feab/Stepup-Middleware-2.1.0-20161026074332Z-6fade533b1516d4c13613cf91ac4963b6919feab.tar.bz2"
+        "https://github.com/SURFnet/Stepup-Middleware/releases/download/2.3.0-20161130154647Z-826187fd113bb97970caad3619bbe974c7e5ab7f/Stepup-Middleware-2.3.0-20161130154647Z-826187fd113bb97970caad3619bbe974c7e5ab7f.tar.bz2"
         "https://github.com/SURFnet/Stepup-SelfService/releases/download/2.2.0-20161018092804Z-8792083b95fe2e073318ba68e4f92329249673ad/Stepup-SelfService-2.2.0-20161018092804Z-8792083b95fe2e073318ba68e4f92329249673ad.tar.bz2"
-        "https://github.com/SURFnet/Stepup-RA/releases/download/2.2.0-20161018093029Z-8dc3497ba0fc70701abeb1c890b6956a80ca24e9/Stepup-RA-2.2.0-20161018093029Z-8dc3497ba0fc70701abeb1c890b6956a80ca24e9.tar.bz2"
+        "https://github.com/SURFnet/Stepup-RA/releases/download/2.4.0-20161130155145Z-bafb84b3ee966990f5b2115d56b04988e7f0cc6b/Stepup-RA-2.4.0-20161130155145Z-bafb84b3ee966990f5b2115d56b04988e7f0cc6b.tar.bz2"
         "https://github.com/SURFnet/Stepup-tiqr/releases/download/release-1.1.4-20161027120418Z-6616dd165903ff4e849b26b755612a3da6fb0409/Stepup-tiqr-release-1.1.4-20161027120418Z-6616dd165903ff4e849b26b755612a3da6fb0409.tar.bz2"
         "https://github.com/SURFnet/oath-service-php/releases/download/1.0.1-20150723081351Z-56c990e62b4ba64ac755ca99093c9e8fce3e8fe9/oath-service-php-1.0.1-20150723081351Z-56c990e62b4ba64ac755ca99093c9e8fce3e8fe9.tar.bz2"
         )

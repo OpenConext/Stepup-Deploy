5ebf18da78b4f540d8c26219087749e267bc7474
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2018-10-19 11:11:35 +0200

    Show more helpfull errors when keyczar is not found, or when using python version > 2

--- scripts/encrypt-file.sh
+++ scripts/encrypt-file.sh
@@ -14,10 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os.path
 import sys
+if sys.version_info[0] > 2:
+    raise Exception("This script requires python 2")
+
+import os.path
 from optparse import OptionParser
-from keyczar import keyczar
+try:
+    from keyczar import keyczar
+except ImportError:
+    raise ImportError('The required "keyczar" module was not found. You can install it using e.g. pip: "pip install python-keyczar"')
 
 parser = OptionParser(usage="usage: %prog [options] <keyczar keystore directory>")
 parser.add_option("-f", "--file", help="the input file", dest="filename", metavar="FILE")
@@ -29,7 +35,7 @@ if len(args) != 1:
   parser.error("wrong number of arguments")
 
 if not os.path.isdir(args[0]):
-  print 'Error: keystore directory "%s" not found' % args[0]
+  print('Error: keystore directory "%s" not found' % args[0])
   sys.exit(1)
 
 if options.filename:
@@ -38,10 +44,10 @@ if options.filename:
     keydir = os.path.expanduser(args[0])
     crypter = keyczar.Crypter.Read(keydir)
     if options.decrypt:
-      print crypter.Decrypt(content)
+      print(crypter.Decrypt(content))
     else:
       encrypted_secret = crypter.Encrypt(content)
-      print encrypted_secret
+      print(encrypted_secret)
 else:
   parser.print_help()
 
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -14,11 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+if sys.version_info[0] > 2:
+    raise Exception("This script requires python 2.")
+
 import os.path
 import getpass
-import sys
 from optparse import OptionParser
-from keyczar import keyczar
+try:
+    from keyczar import keyczar
+except ImportError:
+    raise ImportError('The required "keyczar" module was not found. You can install it using e.g. pip: "pip install python-keyczar"')
 
 parser = OptionParser(usage="usage: %prog [options] <keyczar keystore directory>")
 parser.add_option("-d", "--decrypt", action="store_true", help="decrypt the input", dest="decrypt")
@@ -29,7 +35,7 @@ if len(args) != 1:
   parser.error("wrong number of arguments")
 
 if not os.path.isdir(args[0]):
-  print 'Error: keystore directory "%s" not found' % args[0]
+  print('Error: keystore directory "%s" not found' % args[0])
   sys.exit(1)
 
 keydir = os.path.expanduser(args[0])
@@ -37,8 +43,8 @@ crypter = keyczar.Crypter.Read(keydir)
 
 if options.decrypt:
   encrypted_input = raw_input("Type the encrypted string: ")
-  print 'The decrypted secret: %s' % crypter.Decrypt(encrypted_input)
+  print('The decrypted secret: %s' % crypter.Decrypt(encrypted_input))
 else:
   password = getpass.getpass('Type the secret you want to encrypt: ')
   encrypted_secret = crypter.Encrypt(password)
-  print 'The encrypted secret: %s' % encrypted_secret
+  print('The encrypted secret: %s' % encrypted_secret)
